59be3cc20db4c4c420d9ac9edc4331477cdf0a3c
LeTeguis <teuguiasederis@gmail.com>
2022-10-24 07:49:02 +0100

    Update IA

--- FirstIA.py
+++ FirstIA.py
@@ -31,71 +31,91 @@ class FirstIA(AlgoBase):
         return joueur.taille - joueur.adversaire.taille
 
     def decision_algo(self, joueur, copy_jeu, algo, profondeur):
+        if algo == "mini":
+            return self.decision_mini(joueur, copy_jeu, algo, profondeur)
+
+        return self.auther_decision(joueur, copy_jeu, algo, profondeur)
+
+    def decision_mini(self, joueur, copy_jeu, algo, profondeur):
+        x, y = AlgoBase.decision_algo(self, joueur, copy_jeu, algo, profondeur)
+        if (x, y) == (-1, -1):
+            return -1, -1
+
+        eval = self.infini()
+        pos = (-1, -1)
+        for (i, j) in joueur.possibiliter:
+            e = self.algos[algo](joueur, copy_jeu, i, j, profondeur)
+
+            if eval > e or (eval == e and random.randint(1, 2) == 1):
+                eval = e
+                pos = (i, j)
+
+        return pos[0], pos[1]
+
+    def auther_decision(self, joueur, copy_jeu, algo, profondeur):
         x, y = AlgoBase.decision_algo(self, joueur, copy_jeu, algo, profondeur)
         if (x, y) == (-1, -1):
             return -1, -1
 
         eval = -self.infini()
-        if algo == "mini":
-            eval = self.infini()
         pos = (-1, -1)
         for (i, j) in joueur.possibiliter:
             e = self.algos[algo](joueur, copy_jeu, i, j, profondeur)
 
-            if algo == "mini":
-                if eval > e or (eval == e and random.randint(1, 2) == 1):
-                    eval = e
-                    pos = (i, j)
-            else:
-                if eval < e or (eval == e and random.randint(1, 2) == 1):
-                    eval = e
-                    pos = (i, j)
+            if eval < e or (eval == e and random.randint(1, 2) == 1):
+                eval = e
+                pos = (i, j)
 
         return pos[0], pos[1]
 
     def mini(self, joueur, plateau, i, j, profondeur):
-        print(f"profondeur: {profondeur} | joueur: {joueur.type.name} | ({i}, {j})")
         if profondeur <= 0 or not plateau.logics(joueur, i, j) or plateau.match_terminer():
             return self.evaluation(joueur, plateau, i, j, profondeur)
 
-        mini = self.infini()
+        mini_ = self.infini()
         p_copy = plateau.copy()
-        n_j = None
-        if joueur.type == p_copy.joueur_un.type:
-            n_j = p_copy.joueur_deux
-        else:
-            n_j = p_copy.joueur_un
-        if p_copy.jouer(n_j, i, j):
-            p_copy.vider(joueur)
+        n_j = p_copy.joueur_deux if joueur.type == p_copy.joueur_un.type else p_copy.joueur_un
 
+        if p_copy.simulate(n_j, i, j):
             for (i_, j_) in n_j.possibiliter:
                 e = self.maxi(n_j, p_copy, i_, j_, profondeur - 1)
-                if mini > e or (mini == e and random.randint(1, 2) == 1):
-                    mini = e
-        return mini
+                if mini_ > e or (mini_ == e and random.randint(1, 2) == 1):
+                    mini_ = e
+        return mini_
 
     def maxi(self, joueur, plateau, i, j, profondeur):
-        print(f"profondeur: {profondeur} | joueur: {joueur.type.name} | ({i}, {j})")
         if profondeur <= 0 or not plateau.logics(joueur, i, j) or plateau.match_terminer():
             return self.evaluation(joueur, plateau, i, j, profondeur)
 
-        maxi = -self.infini()
+        maxi_ = -self.infini()
         p_copy = plateau.copy()
-        if p_copy.jouer(joueur, i, j):
-            n_j = None
-            if joueur.type == p_copy.joueur_un.type:
-                n_j = p_copy.joueur_deux
-            else:
-                n_j = p_copy.joueur_un
+        n_j = p_copy.joueur_deux if joueur.type == p_copy.joueur_un.type else p_copy.joueur_un
 
+        if p_copy.simulate(n_j, i, j):
             for (i_, j_) in n_j.possibiliter:
                 e = self.maxi(n_j, p_copy, i_, j_, profondeur - 1)
-                if maxi < e or (maxi == e and random.randint(1, 2) == 1):
-                    maxi = e
-        return maxi
+                if maxi_ < e or (maxi_ == e and random.randint(1, 2) == 1):
+                    maxi_ = e
+        return maxi_
 
-    def minimax(self):
-        pass
+    def minimax(self, joueur, plateau, i, j, profondeur, maximiser=False):
+        if profondeur <= 0 or not plateau.logics(joueur, i, j) or plateau.match_terminer():
+            return self.evaluation(joueur, plateau, i, j, profondeur)
+
+        eval_actuel = -self.infini() if maximiser else self.infini()
+        p_copy = plateau.copy()
+        n_j = p_copy.joueur_deux if joueur.type == p_copy.joueur_un.type else p_copy.joueur_un
+
+        if p_copy.simulate(n_j, i, j):
+            for (i_, j_) in n_j.possibiliter:
+                e = self.minimax(n_j, p_copy, i_, j_, profondeur - 1, not maximiser)
+                if maximiser:
+                    if eval_actuel < e or (eval_actuel == e and random.randint(1, 2) == 1):
+                        eval_actuel = e
+                else:
+                    if eval_actuel > e or (eval_actuel == e and random.randint(1, 2) == 1):
+                        eval_actuel = e
+        return eval_actuel
 
     def nega_max(self):
         pass
